54b4b194ff59881c66e714babbd409855803e0ee
Dariusz Marczewski <marczewski.dariusz@gmail.com>
2019-12-15 17:57:55 +0100

    drobne poprawki

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_pagedown import PageDown
 from flask_uploads import UploadSet, IMAGES, configure_uploads
+from app.main import main, auth, user, book, comment, log
+from app.api import api_bp
+from app import models
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -15,14 +18,11 @@ pagedown = PageDown(app)
 avatars = UploadSet('avatars', IMAGES)
 configure_uploads(app, avatars)
 
-from app.main import main, auth, user, book, comment, log
-from app.api import api_bp
 
 for blueprint in [main, auth, user, book, comment, log, api_bp]:
     app.register_blueprint(blueprint)
 
-from app import models
 
 exists_db = os.path.isfile(app.config['DATABASE'])
 if not exists_db:
-    from . import db_fill
+    from . import database
